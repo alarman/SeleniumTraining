23c9e32b659669f8c53f8ed1bd64deb366264929
Arman <armaniutian@gmail.com>
2024-05-23 02:27:45 +0600

    First commit

---
+++ src/test/java/Selenium/SeleniumFirstProgram.java
@@ -0,0 +1,13 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumFirstProgram {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
